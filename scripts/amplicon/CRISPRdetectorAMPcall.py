e72b4a0fe1d25e2271b66f96d0fca6a5dc79f46a
hlcas <93194094+hlcas@users.noreply.github.com>
2022-09-07 15:06:50 +0800

    Update CRISPRdetectorAMPcall.py

--- scripts/amplicon/CRISPRdetectorAMPcall.py
+++ scripts/amplicon/CRISPRdetectorAMPcall.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 '''
 #-------------------------------------------------
-#	   File Name: CRISPRdetectorCALL.py
+#	   File Name: CRISPRdetectorWGScall.py
 #	   Author: Lei Huang
 #	   Date: 2022.08.20
 #	   E-mail: huanglei192@mails.ucas.ac.cn
@@ -27,21 +27,23 @@ python CRISPRdetectorCALL.py
 --sample: sample name & output directory name [required]
 --o: output path [default:'.']
 --threads: number of threads to run sentieon minimap2 & driver module [default:1] 
---min_allele_frac: the minimum allelic fraction in treated sample [default:0.005] 
---max_fisher_pv_active: the maximum pvalue of the statistical difference between treated and untreated sample [default:0.05]
---amplicons_file: a tab-delimited text amplicons description file with up to 3 columns: AMPLICON_NAME, AMPLICON_SEQ, gRNA_SEQ_without_PAM(optional) [required]  
---min_num_of_reads: the minimum number of reads (per locus site) to evaluate [default:500] 
+--cleavage_offset: center of quantification window to use within respect to the 3-end of the provided sgRNA sequence [default:-3]
+--window_size: defines the size (in bp) of the quantification window extending from the position specified by the cleavage_offset parameter in relation to the provided guide RNA sequence, 0 means whole amplicon analysis [default:0]
 ------------------------------------------------------------------------------------------------------------------------
 '''
 parse = argparse.ArgumentParser(prog='PROG', formatter_class=argparse.RawDescriptionHelpFormatter, description=textwrap.dedent(description))
 parse.add_argument("--o",help='output path',default='.',required=False)
-parse.add_argument("--sample",help="sample name & output dir",required=True)
+parse.add_argument("--sample",help='sample name & output dir',required=True)
 parse.add_argument("--threads",  help="number of threads[15]",default=15,type=int)
-parse.add_argument("--min_allele_frac", help="The minimum allelic fraction in treated sample",default=0.005,type=float)
-parse.add_argument("--max_fisher_pv_active",help="The maximum pvalue of the statistical difference between treated and untreated sample",default=0.05,type=float)
- 
+
 args = parse.parse_args()
 time0 =time.time()
+
+fasta = os.path.abspath(args.assembly)
+
+bed = args.bed
+if bed != 'None':
+	bed = os.path.abspath(bed)
 
 os.chdir(args.o)
 sample = args.sample
@@ -58,23 +60,39 @@ fh.setFormatter(formatter)
 logger.addHandler(fh)
 
 threads = str(args.threads)
-min_frac = str(args.min_allele_frac)
-max_pv = str(args.max_fisher_pv_active)
-
-fasta = 'temp/amplicon_seq.fa'
 
 # Starting calling variants
-param = ' --keep_overlap 1 --resample_depth 100000 --annotation ReadHash --min_tumor_allele_frac '+min_frac+' --filter_t_alt_frac '+min_frac
-if os.path.exists('temp/'+sample+'.control.tmp.bam'):
-	logger.info('Calling variants.')
-	param = param+' --assemble_mode 4 --prune_factor 0 --max_fisher_pv_active '+str(max_pv)
-	os.system('sentieon driver -t '+threads+' -r '+fasta+' -i temp/'+sample + '.tmp.bam -i temp/'+sample+'.control.tmp.bam --algo TNscope --tumor_sample '+sample+' --normal_sample control_'+sample+param+' temp/tmp.vcf.gz && sync')
-	print('sentieon driver -t '+threads+' -r '+fasta+' -i temp/'+sample + '.tmp.bam -i temp/'+sample+'.control.tmp.bam --algo TNscope --tumor_sample '+sample+' --normal_sample control_'+sample+param+' temp/tmp.vcf.gz && sync')
+logger.info('Calling variants.')
 
+bam_t = 'temp/'+sample+'.bam'
+bam_c = 'temp/'+sample+'.control.bam'
+
+# Paired sample: treatment & control
+if bam_c != 0:
+	# Call variants
+	vcf_out = 'temp/raw.paired.vcf.gz'
+	os.system('sentieon driver -i '+bam_t+ ' -i '+bam_c+' -r '+fasta+' --algo EditCounterByAllele '+vcf_out+' && sync')
+	if bed != 'None':
+		# Number of reads
+		logger.info('sentieon driver -i '+bam_t+' -r '+fasta+' --algo EditCounterByTarget --target_list '+bed+' temp/tmp_reads_treatmentxt')
+		os.system('sentieon driver -i '+bam_t+' -r '+fasta+' --algo EditCounterByTarget --target_list '+bed+' temp/tmp_reads_treatment.txt && sync')
+		logger.info('sentieon driver -i '+bam_c+' -r '+fasta+' --algo EditCounterByTarget --target_list '+bed+' temp/tmp_control.txt')
+		os.system('sentieon driver -i '+bam_c+' -r '+fasta+' --algo EditCounterByTarget --target_list '+bed+' temp/tmp_reads_control.txt && sync')
+# Single sample 
 else:
-	logger.info('Calling variants.')			
-	param = param+' --assemble_mode 3 '
-	os.system('sentieon driver -t '+threads+' -r '+fasta+' -i temp/'+sample+'.tmp.bam --algo TNscope --tumor_sample '+sample+param+'temp/tmp.vcf.gz && sync')
+	vcf_out = 'temp/raw.treatment.vcf.gz'
+	if bed != None:
+		# Call variants and calculate number of reads mapped
+		logger.info('sentieon driver -i '+bam_t+' -r '+fasta+' --algo EditCounterByAllele '+vcf_out+' --algo EditCounterByTarget --target_list '+bed+' temp/tmp_reads_treatment.txt')
+		os.system('sentieon driver -i '+bam_t+' -r '+fasta+' --algo EditCounterByAllele '+vcf_out+' --algo EditCounterByTarget --target_list '+bed+' temp/tmp_reads_treatment.txt && sync')
+	else:
+		# Call variants
+		logger.info('sentieon driver -i '+bam_t+' -r '+fasta+' --algo EditCounterByAllele '+vcf_out)
+		os.system('sentieon driver -i '+bam_t+' -r '+fasta+' --algo EditCounterByAllele '+vcf_out+' && sync')
+
+logger.info('bcftools norm -m- '+vcf_out+' > temp/split.vcf')
+os.system('bcftools norm -m- '+vcf_out+' > temp/split.vcf && sync')
+
 
 logger.info('Finished : variants called.')
 
