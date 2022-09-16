006fb09cff4c8aafe90529e365a4abcbf942ee7c
hlcas <93194094+hlcas@users.noreply.github.com>
2022-09-16 18:20:36 +0800

    Update CRISPRdetectorAMPstat.py

--- scripts/amplicon/CRISPRdetectorAMPstat.py
+++ scripts/amplicon/CRISPRdetectorAMPstat.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 '''
 #-------------------------------------------------
-#	   File Name: CRISPRdetectorSTAT.py
+#          Filename: CRISPRdetectorWGSstat.py
 #	   Description: The script is designed to analyze deep-sequencing PCR products, aiming to compute CRISPR-triggered on/off-target efficiency.
 #	   Author: Lei Huang
 #	   Date: 2022.08.20
@@ -18,60 +18,71 @@ import textwrap
 import numpy as np
 import pandas as pd
 from Bio.Seq import Seq
+from scipy import stats
 from pyfaidx import Fasta
 
 description = '''
 ------------------------------------------------------------------------------------------------------------------------
 This script is designed to analyze single amplicon & pooled amplicons sequencing data.
 Usage:
-python CRISPRdetectorAMP.py  
+python CRISPRdetectorWGSstat.py  
 --sample: sample name & output directory name [required]
+--o: output path [default='.']
+--filt: To filt out background variants applying Chi-square test (1) or not (0) [default=1]
+--max_pv_active: The maximum pvalue of the statistical difference between treatment and control group sample [default=0.05]
 ------------------------------------------------------------------------------------------------------------------------
 '''
 
 parse = argparse.ArgumentParser(prog='PROG', formatter_class=argparse.RawDescriptionHelpFormatter, description=textwrap.dedent(description))
 parse.add_argument("--sample",help="sample name & output dir",required=True)
 parse.add_argument("--o",help='output path',default='.',required=False)
-parse.add_argument("--threads",  help="number of threads[15]",default=15,type=int)
-parse.add_argument("--ignore_substitutions",help="Enable substitutions evaluation[1]",default=0,type=int)
-parse.add_argument("--min_num_of_reads",help="The minimum number of reads (per locus site) to evaluate",default=500,type=int)
- 
+parse.add_argument("--min_num_of_reads",help="The minimum number of reads (per locus site) to evaluate",default=100,type=int)
+parse.add_argument("--filt",help='To filt out background variants applying Chi-square test [1] or not [0]',default=1,required=False)
+parse.add_argument("--max_pv_active",help="The maximum pvalue of the statistical difference between treatment and control group sample",default=0.05,type=float,required=False)
+
 args = parse.parse_args()
 time0 =time.time()
 
+min_num_of_reads = args.min_num_of_reads
+# sample name & output path
 sample = args.sample
 os.chdir(args.o)
 os.chdir(sample)
 
+
+# LOG file format
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 rq = time.strftime('%Y%m%d%H%M', time.localtime(time.time()))
-fh = logging.FileHandler('main.log', mode='w')
+fh = logging.FileHandler('RUNNING_statistics.log', mode='w')
 fh.setLevel(logging.DEBUG)
 formatter = logging.Formatter("%(asctime)s - %(levelname)s: %(message)s")
 fh.setFormatter(formatter)
 logger.addHandler(fh)
 
-threads = str(args.threads)
-min_num_of_reads = args.min_num_of_reads
-
-fasta = os.path.abspath('temp/amplicon_seq.fa')
-amplicon_fas = Fasta(fasta)
-
-# Numbers of reads mapped to each amplicon
+# Numbers of reads mapped to each region
 mapdf = pd.read_csv('temp/tmp_reads_treatment.txt',sep='\t',header=None)
 mapdf.columns = ['amplicon','total_reads_treatment','edited_reads_treatment']
 mapdf = mapdf[['amplicon','total_reads_treatment']]
 if os.path.exists('temp/tmp_reads_control.txt'):
+	sample_list = ['treatment','control']
 	mapdf_c = pd.read_csv('temp/tmp_reads_control.txt',sep='\t',header=None)
 	mapdf_c.columns = ['amplicon','total_reads_control','edited_reads_control']
 	mapdf_c = mapdf_c[['amplicon','total_reads_control']]
 	mapdf = pd.merge(mapdf,mapdf_c,on='amplicon',how='left')
+else:
+	sample_list = ['treatment']
 
+# Filt out Region without required depth of reads
 loci_low_reads = list(mapdf[mapdf['total_reads_treatment'] < min_num_of_reads]['amplicon'].values)
 mapdf = mapdf[mapdf['total_reads_treatment'] >= min_num_of_reads]
+
+if len(mapdf) == 0:
+	sys.exis(0)
+	
 loci_high_reads = mapdf['amplicon'].values
 
+# REF & ALT
 def diff_len(x,y):
 	if x == '-':
 		return len(y)
@@ -81,13 +92,13 @@ def diff_len(x,y):
 		else:
 			return len(y) - len(x)
 
-def vcflencheck(vcfx,samx):
-	if len(vcfx) == 0:
-		logger.info('No variants called for '+samx+' group data.')
+# Check if the length of the VCF
+def vcflencheck(dfx,strx):
+	if len(dfx) == 0:
+		logger.info(strx)
 		time1=time.time()
 		logger.info('Finished! Running time: %s seconds'%(round(time1-time0,2)))
-		if samx == 'treatment':
-			sys.exit(0)
+		sys.exit(0)	
 
 def extract_value(x,y,z):
 	if x in y.keys():
@@ -95,64 +106,110 @@ def extract_value(x,y,z):
 	else:
 		return 0
 
-if os.path.exists('temp/split.control.vcf'):
-	sample_list = ['treatment','control']
+def chi_test(xReadHash,yReadHash,xReadNums,yReadNums):
+	xSplit = xReadHash.split('|')
+	ySplit = yReadHash.split('|')
+	if (xSplit == ['']) or (ySplit == ['']):
+		return 0
+	else:
+		a0 = len(xSplit)
+		a1 = len(ySplit)
+		a2 = xReadNums - a0
+		a3 = yReadNums - a1
+		if (a0 < 5) or (a1 < 5) or (a2 < 5) or (a3 < 5):
+			return stats.chi2_contingency([[a0,a2],[a1,a3]],correction=True)[1]
+		else:
+			return stats.chi2_contingency([[a0,a2],[a1,a3]],correction=False)[1]
+
+vcf = pd.read_csv('temp/raw.vcf.gz',sep='\t',header=None,comment='#',compression='gzip')
+if len(sample_list) == 2:
+	vcf.columns = ['#CHROM','POS','ID','REF','ALT','QUAL','FILTER','INFO','FORMAT','treatment','control']
 else:
-	sample_list = ['treatment']
+	vcf.columns = ['#CHROM','POS','ID','REF','ALT','QUAL','FILTER','INFO','FORMAT','treatment']
+
+vcf = vcf[vcf['#CHROM'].isin(loci_high_reads)]
+vcflencheck(vcf,'Loci with enough reads called no variants.!')
+
+vcf['FORMAT'] = 'GT:ReadHash'
+for i in sample_list:    
+	vcf[i] = vcf[i].apply(lambda x:x.split(':')[-1])
+
+with open('temp/split.vcf','w') as f:
+	for i in range(len(vcf)):
+		chrx = vcf['#CHROM'].values[i]
+		pos = str(vcf['POS'].values[i])
+		ref = vcf['REF'].values[i]
+		alt = vcf['ALT'].values[i].split(',')
+		treadhash = vcf['treatment'].values[i].split(',')
+		if 'N' not in ref:
+			if len(sample_list) == 2:
+				creadhash = vcf['control'].values[i].split(',')
+				for j in range(len(alt)):
+					if 'N' not in alt[j]:
+						f.write(chrx+'\t'+pos+'\t.\t'+ref+'\t'+alt[j]+'\t.\t.\t.\tGT:ReadHash\t0/1:'+treadhash[j]+'\t0/1:'+creadhash[j]+'\n')
+					elif alt[j] == '<NON_REF>':
+						f.write(chrx+'\t'+pos+'\t.\t'+ref[0]+'\t'+alt[j]+'\t.\t.\t.\tGT:ReadHash\t0/1:'+treadhash[j]+'\t0/1:'+creadhash[j]+'\n')
+			else:
+				for j in range(len(alt)):
+					if 'N' not in alt[j]:# or (alt[j] == '<NON_REF>'):
+						f.write(chrx+'\t'+pos+'\t.\t'+ref+'\t'+alt[j]+'\t.\t.\t.\tGT:ReadHash\t0/1:'+treadhash[j]+'\n')
+					elif alt[j] == '<NON_REF>':
+						f.write(chrx+'\t'+pos+'\t.\t'+ref[0]+'\t'+alt[j]+'\t.\t.\t.\tGT:ReadHash\t0/1:'+treadhash[j]+'\n')
+
+vcf = pd.read_csv('temp/split.vcf',sep='\t',header=None,comment='#')
+
+if len(sample_list) == 2:
+	vcf.columns = ['#CHROM','POS','ID','REF','ALT','QUAL','FILTER','INFO','FORMAT','treatment','control']
+else:
+	vcf.columns = ['#CHROM','POS','ID','REF','ALT','QUAL','FILTER','INFO','FORMAT','treatment']
+
+# window.bed
+df_window = pd.read_csv('temp/window.bed',sep='\t',header=None)
+df_window.columns = ['amplicon','window_start','window_end','tmp']
+df_window = df_window[['amplicon','window_start','window_end']]
+
+vcf.to_csv('temp/anno.vcf',sep='\t',index=None)
+os.system('convert2annovar.pl -format vcf4 temp/anno.vcf > temp/anno.avinput && sync')
+
+dfin = pd.read_csv('temp/anno.avinput',sep='\t',header=None)
+dfin.columns = ['amplicon','Start','End','Ref','Alt','tmp1','tmp2','tmp3']
+dfin = dfin[['amplicon','Start','End','Ref','Alt']]
+
+dfin = pd.merge(dfin,mapdf,on='amplicon',how='left')
+
+# ReadHash
+for i in sample_list:
+	dfin[i+'_ReadHash'] = vcf[i].apply(lambda x:x[4:])
+
+if len(sample_list) == 2:
+	if args.filt == 1:
+		dfin['pvalue'] = dfin.apply(lambda row:chi_test(row['treatment_ReadHash'],row['control_ReadHash'],row['total_reads_treatment'],row['total_reads_control']),axis=1)
+		dfin.to_csv('temp/out_mutations.txt',index=None,sep='\t')
+		dfin = dfin[dfin['pvalue'] < args.max_pv_active]
+		dfin.to_csv('temp/out_mutations_pvalue_Filt.txt',index=None,sep='\t')
+
+# SV or other type of variants
+dfsv = dfin[dfin['Alt'] == '<NON_REF>']
+dfin = dfin[dfin['Alt'] != '<NON_REF>']
+dfin['diff_len'] = dfin.apply(lambda row:diff_len(row['Ref'],row['Alt']),axis=1)
+
+amplicon_fas = Fasta('temp/amplicon_seq.fa')
 
 for i in sample_list:
-	try:
-		vcf = pd.read_csv('temp/split.'+i+'.vcf',sep='\t',header=None,comment='#')
-	except:
-		logger.info('No variants called for '+i+' group data.')
-		if i == 'treatment':
-			sys.exit(0)
-
-	vcf.columns = ['#CHROM','POS','ID','REF','ALT','QUAL','FILTER','INFO','FORMAT',i]
-	# Filter out sites with reads number lower than input parameter: min_mum_of_reads
-	vcf = vcf[vcf['#CHROM'].isin(loci_high_reads)]
-	vcflencheck(vcf,i)
-
-	vcf['FORMAT'] = 'GT:ReadHash'
-	# GT:ReadHash
-	vcf[i] = vcf[i].apply(lambda x:'0/1:'+x.split(':')[-1])
-	vcf = vcf.reset_index(drop=True)
-	# anno.treatment.vcf
-	vcf.to_csv('temp/anno.'+i+'.vcf',index=None,sep='\t')
-	# anno.treatment.avinput
-	os.system('convert2annovar.pl -format vcf4 temp/anno.'+i+'.vcf > temp/anno.'+i+'.avinput && sync')
-	dfin = pd.read_csv('temp/anno.'+i+'.avinput',sep='\t',header=None)
-	dfin.columns = ['Site','Start','End','Ref','Alt','tmp1','tmp2','tmp3']
-	dfin = dfin[['Site','Start','End','Ref','Alt']]
-	dfin['ReadHash'] = vcf[i].apply(lambda x:x[4:])
-
-	# NON_REF: SVs or unknown type mutations
-	dfsv = dfin[dfin['Alt'] == '<NON_REF>']
-	dfin = dfin[dfin['Alt'] != '<NON_REF>']
-	vcflencheck(dfin,i)
-
-	# Length difference between Ref and ALT
-	dfin['diff_len'] = dfin.apply(lambda row:diff_len(row['Ref'],row['Alt']),axis=1)
-
-	# window.bed
-	df_window = pd.read_csv('temp/window.bed',sep='\t',header=None)
-	df_window.columns = ['amplicon','window_start','window_end','tmp']
-
 	SiteMut = {}
-	for j in dfin['Site'].unique():
+	for j,dfinJ in dfin.groupby('amplicon'):
 		SiteMut[j] = {}
 		os.system('mkdir -p '+j)
 		dfJ_window = df_window[df_window['amplicon'] == j]
 		window_start = dfJ_window['window_start'].values[0]
-		window_end = dfJ_window['window_end'].values[0]
+		window_end_1 = dfJ_window['window_end'].values[0] + 1
 		ampSeq = str(amplicon_fas[j][:].seq)
 		amplen = len(ampSeq)
 		amplen_1 = amplen + 1
-		amp_Reads = mapdf[mapdf['amplicon'] == j]['total_reads_'+i].values[0]
+		amp_Reads = dfinJ['total_reads_'+i].values[0]
 		# Dataframe for each site
-		dfinJ = dfin[dfin['Site'] == j]
 		if len(dfsv) != 0:
-			dfsvJ = dfsv[dfsv['Site'] == j]
+			dfsvJ = dfsv[dfsv['amplicon'] == j]
 
 		# Indel size ReadHash
 		IndelSize_ReadHash = {}			
@@ -162,10 +219,10 @@ for i in sample_list:
 			for l,d in df_indel.groupby('diff_len'):
 				IndelSize_ReadHash[l] = []
 				for v in range(len(d)):
-					var_start = d['Start'].values[v]
-					var_end = d['End'].values[v]
-					if len(set(range(window_start,window_end+1)).intersection(set(range(var_start,var_end+1)))) != 0:
-						splitReadHash = d['ReadHash'].values[v].split('|')
+					var_start = d['Start'].values[v] 
+					var_end_1 = d['End'].values[v] + 1
+					if len(set(range(window_start,window_end_1)).intersection(set(range(var_start,var_end_1)))) != 0:
+						splitReadHash = d[i+'_ReadHash'].values[v].split('|')
 						IndelSize_ReadHash[l] = IndelSize_ReadHash[l] + splitReadHash
 						total_indel_nums += len(splitReadHash)
 
@@ -176,7 +233,7 @@ for i in sample_list:
 					f.write(str(t)+'\t'+str(round(len(IndelSize_ReadHash[t])*100/total_indel_nums,2))+'\n')
 
 		# POS mutations ReadHash		
-		POS_indel = {}
+		POS_INDEL = {}
 		POS_MUT = {}
 		POS_DEL = {}
 		POS_INS = {}
@@ -198,11 +255,11 @@ for i in sample_list:
 			dlen = dfinJ['diff_len'].values[n]
 			startPOS = dfinJ['Start'].values[n]
 			#print(str(j)+':'+str(len(dfinJ))+':'+str(n))
-			ReadHashs = dfinJ['ReadHash'].values[n].split('|')
+			ReadHashs = dfinJ[i+'_ReadHash'].values[n].split('|')
 			# Deletions
 			if dlen < 0:
-				endPOS = dfinJ['End'].values[n]
-				for m in range(startPOS,endPOS+1):
+				endPOS_1 = dfinJ['End'].values[n] + 1
+				for m in range(startPOS,endPOS_1):
 					POS_DEL[m] = set(POS_DEL[m]) | set(ReadHashs)
 			# Insertions
 			elif dlen > 0:
@@ -220,13 +277,11 @@ for i in sample_list:
 					POS_C[startPOS] = set(POS_C[startPOS]) | set(ReadHashs)
 				elif altN == 'T':
 					POS_T[startPOS] = set(POS_T[startPOS]) | set(ReadHashs)
-				else: # 'N'
-					pass
-	
+				
 		for t in range(1,amplen_1):
 			POS_SUB[t] = set(POS_A[t]) | set(POS_G[t]) | set(POS_C[t]) | set(POS_T[t])
-			POS_indel[t] = set(POS_DEL[t]) | set(POS_INS[t])
-			POS_MUT[t] = set(POS_indel[t]) | set(POS_SUB[t])
+			POS_INDEL[t] = set(POS_DEL[t]) | set(POS_INS[t])
+			POS_MUT[t] = set(POS_INDEL[t]) | set(POS_SUB[t])
 
 		POS_NUMS = {}	
 		POS_FREQ = {}
@@ -247,7 +302,7 @@ for i in sample_list:
 					POS_NUMS[t]['G'] = len(POS_G[t])
 					POS_NUMS[t]['C'] = len(POS_C[t])
 					POS_NUMS[t]['T'] = len(POS_T[t])
-					POS_NUMS[t]['Indel'] = len(POS_indel[t])
+					POS_NUMS[t]['Indel'] = len(POS_INDEL[t])
 					POS_NUMS[t][POS_ref] = amp_Reads - POS_NUMS[t]['S'] - POS_NUMS[t]['D']
 					
 					POS_FREQ[t] = {}
@@ -270,19 +325,30 @@ for i in sample_list:
 		WINDOW_INDEL = []
 		WINDOW_MUT0 = []
 		WINDOW_MUT1 = []
-		for t in range(window_start,window_end+1):
+		WINDOW_NON_REF = []
+
+		for t in range(window_start,window_end_1):
 			WINDOW_SUB = set(WINDOW_SUB) | set(POS_SUB[t])
 			WINDOW_DEL = set(WINDOW_DEL) | set(POS_DEL[t])
 			WINDOW_INS = set(WINDOW_INS) | set(POS_INS[t])
-			WINDOW_INDEL = set(WINDOW_INDEL) | set(POS_indel[t])
+			WINDOW_INDEL = set(WINDOW_INDEL) | set(POS_INDEL[t])
 			WINDOW_MUT0 = set(WINDOW_MUT0) | set(POS_MUT[t])
 
 		if len(dfsvJ) != 0:
 			for t in range(len(dfsvJ)):
 				sv_start = dfsvJ['Start'].values[t]
-				sv_end = dfsvJ['End'].values[t]
-				if len(set(range(window_start,window_end+1)).intersection(set(range(sv_start,sv_end+1)))) != 0:
-					WINDOW_MUT1 = set(WINDOW_MUT0) | set(dfsvJ['ReadHash'].values[t].split('|'))
+				sv_end_1 = dfsvJ['End'].values[t] + 1
+				if len(set(range(window_start,window_end_1)).intersection(set(range(sv_start,sv_end_1)))) != 0:
+					WINDOW_MUT1 = set(WINDOW_MUT1) | set(WINDOW_MUT0) | set(dfsvJ[i+'_ReadHash'].values[t].split('|'))
+					WINDOW_NON_REF = set(WINDOW_NON_REF) | set(dfsvJ[i+'_ReadHash'].values[t].split('|'))
+			try:
+				WINDOW_MUT1.remove('')
+				WINDOW_NON_REF.remove('')
+			except:
+				pass
+
+		else:
+			WINDOW_MUT1 = WINDOW_MUT0
 
 		SiteMut[j]['S'] = len(WINDOW_SUB)
 		SiteMut[j]['D'] = len(WINDOW_DEL)
@@ -290,16 +356,21 @@ for i in sample_list:
 		SiteMut[j]['INDEL'] = len(WINDOW_INDEL)
 		SiteMut[j]['MUT0'] = len(WINDOW_MUT0)
 		SiteMut[j]['MUT1'] = len(WINDOW_MUT1)
+		SiteMut[j]['NON_REF'] = len(WINDOW_NON_REF)
 		
 	mapdf[i+'_Substitutions'] = mapdf['amplicon'].apply(lambda x:extract_value(x,SiteMut,'S'))
 	mapdf[i+'_Deletions'] = mapdf['amplicon'].apply(lambda x:extract_value(x,SiteMut,'D'))
 	mapdf[i+'_Insertions'] = mapdf['amplicon'].apply(lambda x:extract_value(x,SiteMut,'I'))
 	mapdf[i+'_Indels'] = mapdf['amplicon'].apply(lambda x:extract_value(x,SiteMut,'INDEL'))
-	mapdf[i+'_Modified_0'] = mapdf['amplicon'].apply(lambda x:extract_value(x,SiteMut,'MUT0'))
-	mapdf[i+'_Modified_1'] = mapdf['amplicon'].apply(lambda x:extract_value(x,SiteMut,'MUT1'))
+	mapdf[i+'_NON_REF'] = mapdf['amplicon'].apply(lambda x:extract_value(x,SiteMut,'NON_REF'))
+	mapdf[i+'_Modified_without_NON_REF'] = mapdf['amplicon'].apply(lambda x:extract_value(x,SiteMut,'MUT0'))
+	mapdf[i+'_Modified_with_NON_REF'] = mapdf['amplicon'].apply(lambda x:extract_value(x,SiteMut,'MUT1'))
 
-	for k in ['Substitutions','Deletions','Insertions','Indels','Modified_0','Modified_1']:
+	for k in ['Substitutions','Deletions','Insertions','Indels','NON_REF','Modified_without_NON_REF','Modified_with_NON_REF']:
 		mapdf[i+'_'+k+'%'] = mapdf[i+'_'+k]/mapdf['total_reads_'+i]
 		mapdf[i+'_'+k+'%'] = mapdf[i+'_'+k+'%'].apply(lambda x:round(x*100,2))
 
 mapdf.to_csv('out_result_summary.txt',sep='\t',index=None)
+
+time1=time.time()
+logger.info('Finished! Running time: %s seconds'%(round(time1-time0,2))+'.')
