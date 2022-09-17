c8d76a87303c7ed2943d5cfe72e9f221de7bcf61
hlcas <93194094+hlcas@users.noreply.github.com>
2022-09-17 18:47:50 +0800

    Update CRISPRdetectorWGSanno.py

--- scripts/WGS/CRISPRdetectorWGSanno.py
+++ scripts/WGS/CRISPRdetectorWGSanno.py
@@ -16,52 +16,39 @@ import argparse
 import textwrap
 import numpy as np
 import pandas as pd
-from Bio.Seq import Seq
-from pyfaidx import Fasta
 
 description = '''
-------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------
 This script is designed to call variants for whole genome sequencing data.
 Usage:
 python CRISPRdetectorWGS_TNscope.py  
 --o: output path [default='.']
---bed: BED format file path [optional]
---threads: number of threads [default=1]
-#--assembly: reference genome assembly path [required]
+--bed: BED format file path [required]
+--db: ANNOVAR database path [required]
+--assembly: assembly version, hg19,hg38 ... [required]
 --sample: sample name & output directory name [required]
---assembly: assembly version, hg19,hg38 ... [optional]
---db: ANNOVAR database path [optional]
---ClinVar: only organism homo sapiens experiment type sequencing data support variant annotations from ClinVar [default:0]  
-------------------------------------------------------------------------------------------------------------------------
+--min_num_of_reads: The minimum number of reads (per site) to evaluate [default=0] 
+--ClinVar: only organism homo sapiens experiment type sequencing data support variant annotations from ClinVar [default=0]  
+--------------------------------------------------------------------------------------------------------------------------
 '''
 
 parse = argparse.ArgumentParser(prog='PROG', formatter_class=argparse.RawDescriptionHelpFormatter, description=textwrap.dedent(description))
+parse.add_argument("--bed",help='BED format file path',required=True)
+parse.add_argument("--db", help="Annovar database path",required=True)
 parse.add_argument("--o",help='output path',default='.',required=False)
 parse.add_argument("--sample",help='sample name & output dir',required=True)
-#parse.add_argument("--threads",  help="number of threads [15]",default=15,type=int)
-#parse.add_argument("--assembly",help='reference genome assembly path',required=True)
-parse.add_argument("--bed",help='BED format file path',default='None',required=False)
-#parse.add_argument("--min_tumor_allele_frac", help="The minimum allelic fraction in treated sample",default=0.005,type=float)
-#parse.add_argument("--max_fisher_pv_active",help="The maximum pvalue of the statistical difference between treated and untreated sample",default=0.05,type=float)
+parse.add_argument("--assembly", help="assembly version, hg38/hg19/mm9...",required=True)
 parse.add_argument("--min_num_of_reads",help="The minimum number of reads (per site) to evaluate",default=0,type=int)
-parse.add_argument("--db", help="Annovar database path",required=False,default='/data/toolkit/annovar/')
-parse.add_argument("--assembly", help="assembly version",required=False)
-parse.add_argument("--ClinVar", help="Organism Homo sapiens experiment type sequencing data support variant annotations from ClinVar[1]", default=0, type = int)
+parse.add_argument("--ClinVar", help="Organism Homo sapiens experiment type sequencing data support variant annotations from ClinVar[1]",default=0,type=int)
 
 args = parse.parse_args()
 time0 =time.time()
 
-#fa = os.path.abspath(args.assembly)
-
-bed = args.bed
-if bed != 'None':
-	bed = os.path.abspath(bed)
-
+bed = os.path.abspath(args.bed)
 os.chdir(args.o)
 sample = args.sample
 os.chdir(sample)
 
-# log file format
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 rq = time.strftime('%Y%m%d%H%M', time.localtime(time.time()))
@@ -74,8 +61,6 @@ logger.addHandler(fh)
 min_num_of_reads = args.min_num_of_reads
 annovar_db = args.db
 assembly = args.assembly
-
-#t = str(args.threads)
 
 # Check the length of the VCF
 def vcflencheck(dfx,strx):
@@ -99,21 +84,21 @@ else:
 	sys.exit('Please check the path of VCF format file called by TNscope.')
 
 mapdf = pd.read_csv('temp/tmp_reads_treatment.txt',sep='\t',header=None)
-mapdf.columns = ['region','total_reads_treatment','edited_reads_treatment']
-mapdf = mapdf[['region','total_reads_treatment']]
-loci_low_reads = list(mapdf[mapdf['total_reads_treatment'] < min_num_of_reads]['region'].values)
+mapdf.columns = ['Site','total_reads_treatment','edited_reads_treatment']
+mapdf = mapdf[['Site','total_reads_treatment']]
+loci_low_reads = list(mapdf[mapdf['total_reads_treatment'] < min_num_of_reads]['Site'].values)
 mapdf = mapdf[mapdf['total_reads_treatment'] >= min_num_of_reads]
 
 if len(mapdf) == 0:
 	logger.info('Not enough reads mapped to BED format file defined region.')
 	sys.exit('Not enough reads mapped to BED format file defined region.')
-loci_high_reads = mapdf['region'].values
+loci_high_reads = mapdf['Site'].values
 
 # window.bed
 df_window = pd.read_csv(bed,sep='\t',header=None)
-df_window.columns = ['#CHROM','window_start','window_end','region']
+df_window.columns = ['#CHROM','window_start','window_end','Site']
 
-df_window = df_window[df_window['region'].isin(loci_high_reads)]
+df_window = df_window[df_window['Site'].isin(loci_high_reads)]
 
 # Filt out reads not on BED file defined chromosome
 window_chr = df_window['#CHROM'].unique()
@@ -141,11 +126,12 @@ os.system('convert2annovar.pl -format vcf4 temp/tmp.annovar.vcf > temp/annovar.t
 
 dfin = pd.read_csv('temp/annovar.tab',sep='\t',header=None)
 dfin.columns = ['#CHROM','Start','End','Ref','Alt','tmp1','tmp2','tmp3']
+vcf = vcf.reset_index(drop=True)
 
 Chr_POS = {}
 for i in range(len(df_window)):
 	chrI = df_window['#CHROM'].values[i]
-	regionI = df_window['region'].values[i]
+	regionI = df_window['Site'].values[i]
 	startI = df_window['window_start'].values[i]
 	endI = df_window['window_end'].values[i]
 	if chrI not in Chr_POS:
@@ -155,9 +141,11 @@ for i in range(len(df_window)):
 	for j in range(startI,endI+1):
 		Chr_POS[chrI][regionI].append(j)
 
-dfin['region'] = dfin.apply(lambda row:inter(row['Start'],row['End'],row['#CHROM']),axis=1)
-dfin = dfin[dfin['region'] != 0]
-del dfin['region']
+dfin['Site'] = dfin.apply(lambda row:inter(row['Start'],row['End'],row['#CHROM']),axis=1)
+dfin = dfin[dfin['Site'] != 0]
+dfin_0 = dfin[['Site']]
+
+del dfin['Site']
 vcflencheck(dfin,'No variants on BED file defined regions.')
 dfin.to_csv('temp/annovar.tab',sep='\t',header=None,index=None)
 
@@ -169,6 +157,13 @@ else:
 	os.system('table_annovar.pl temp/annovar.tab '+annovar_db+' -buildver '+assembly+' -out temp/out -remove -protocol refGene -operation g  -nastring . -csvout -polish && sync')
 	
 	#csvout = pd.read_csv('temp/out.'+assembly+'_multianno.csv')
+
+vcf = vcf.drop(['#CHROM','POS','ID','REF','ALT'],axis=1)
+df_anno = pd.read_csv('temp/out.'+assembly+'_multianno.csv')
+df_anno_0 = df_anno[df_anno.columns[0:4]]
+df_anno_1 = df_anno[df_anno.columns[4:]]
+df_out = pd.concat([dfin_0,df_anno_0,vcf,df_anno_1],axis=1)
+df_out.to_excel('temp/out.'+assembly+'_multianno.xlsx',index=None)
 
 time1=time.time()
 logger.info('Finished! Running time: %s seconds'%(round(time1-time0,2))+'.')
